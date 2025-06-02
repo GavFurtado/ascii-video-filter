f270175eea6601366cbb67f566427c09a90d0fe9
Gavin <gavin.furtado12@gmail.com>
2025-06-02 18:42:29 +0530

    err

--- main.cpp
+++ main.cpp
@@ -1,22 +1,18 @@
 #pragma once
 
 #include <algorithm>
-#include <array>
+#include <cstddef>
 #include <filesystem>
 #include <iostream>
 #include <string>
+#include <vector>
 
 inline bool validateArgs(int argc, int expected) {
     return argc == expected;
 }
 
-bool checkExtension(const std::filesystem::path &path) {
-    const std::array<std::string, 7> extensions = {
-        ".mp4", ".mkv", ".avi", ".mov", ".flv", ".webm", ".wmv"
-    };
-
+bool validateExtension(const std::filesystem::path &path, const std::vector<std::string> &extensions) {
     std::string ext = path.extension().string();
-    // convert extension to lower
     std::transform(ext.begin(), ext.end(), ext.begin(), [](unsigned char c) {
         return std::tolower(c);
     });
@@ -32,15 +28,28 @@ bool checkExtension(const std::filesystem::path &path) {
 int main(int argc, const char *argv[]) {
     // very likely to change depending on scope
     int expected = 1;
+    const std::vector<std::string> extensions = {
+        ".mp4", ".mkv", ".avi", ".mov", ".flv", ".webm", ".wmv"
+    };
+
 
     if(!validateArgs(argc, expected)) {
-        std::cerr << "Invalid Argument Count. Too many or too few.\nSyntax: AsciiVideoFilter filename";
+        std::cerr << "Invalid Argument Count. Too many or too few.\nExpected: AsciiVideoFilter filename\n";
         return -1;
     }
 
     std::filesystem::path path = argv[1];
 
-    if(!checkExtension(path))
+    if(!validateExtension(path, extensions)) {
+        std::cerr << "File type invalid.\nValid file types: ";
+        for(size_t i = 0; i <= extensions.size() - 2; ++i) {
+            std::cerr << extensions[i] << ", ";
+        }
+        std::cerr << extensions.back() << "\n";
+        return -1;
+    }
+
+    // start processing the video
 
 
     return 0;
