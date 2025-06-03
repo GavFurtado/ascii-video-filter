d5586b69c0ead9499e168796b1bb661802672f39
Gavin2 <gavin.furtado12@gmail.com>
2025-06-03 09:57:44 +0530

    erm

--- main.cpp
+++ main.cpp
@@ -1,19 +1,49 @@
 #pragma once
 
 #include <algorithm>
-#include <array>
 #include <filesystem>
 #include <iostream>
 #include <string>
+#include <vector>
+
+inline bool validateArgs(int argc, int expected);
+bool checkExtension(const std::filesystem::path &path, const std::vector<std::string> &extensions);
+
+
+int main(int argc, const char *argv[]) {
+    // very likely to change depending on scope
+    int expected = 1;
+    const std::vector<std::string> extensions = {
+        ".mp4", ".mkv", ".avi", ".mov", ".flv", ".webm", ".wmv"
+    };
+
+    if(!validateArgs(argc, expected)) {
+        std::cerr << "Invalid Argument Count. Too many or too few.\nSyntax: AsciiVideoFilter filename";
+        return -1;
+    }
+
+    std::filesystem::path path = argv[1];
+
+    if(!checkExtension(path, extensions)) {
+        std::cerr << "Invalid Media File type. Supported types: "<< std::endl;
+        for(const auto& extension : extensions) {
+            std::cerr << extension << ", ";
+        }
+        std::cerr << std::endl;
+    }
+
+
+    // ffmpeg stuff
+
+    return 0;
+}
 
 inline bool validateArgs(int argc, int expected) {
     return argc == expected;
 }
 
-bool checkExtension(const std::filesystem::path &path) {
-    const std::array<std::string, 7> extensions = {
-        ".mp4", ".mkv", ".avi", ".mov", ".flv", ".webm", ".wmv"
-    };
+bool checkExtension(const std::filesystem::path &path, const std::vector<std::string> &extensions) {
+
 
     std::string ext = path.extension().string();
     // convert extension to lower
@@ -27,22 +57,4 @@ bool checkExtension(const std::filesystem::path &path) {
         }
     }
     return false;
-}
-
-int main(int argc, const char *argv[]) {
-    // very likely to change depending on scope
-    int expected = 1;
-
-    if(!validateArgs(argc, expected)) {
-        std::cerr << "Invalid Argument Count. Too many or too few.\nSyntax: AsciiVideoFilter filename";
-        return -1;
-    }
-
-    std::filesystem::path path = argv[1];
-
-    if(!checkExtension(path))
-
-
-    return 0;
-
 }
